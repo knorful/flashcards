c57adb72caea3354aea8d7cb579f05c54a1ac684
Kris Norful <krisnorful@gmail.com>
2021-04-01 11:17:42 -0500

    create selector function to select addTopic reducer from topicsSlice

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,17 +1,22 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const topicsSlice = createSlice({
   name: "topics",
   initialState: {
     topics: {}
   },
-  reducer: {
+  reducers: {
     addTopic(state, action) {
+      console.log("action", action);
       state.topics = { topics: { [action.payload.id]: action.payload } };
       state.topics.quizIds = [];
     }
   }
 });
 
-export const selectTopics = (state) => state.topics.topics;
+export const selectTopics = (state) => {
+  return state.topics;
+};
+
+export const { addTopic } = topicsSlice.actions;
 export default topicsSlice.reducer;
