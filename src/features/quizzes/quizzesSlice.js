5b56eb5f1df7cdbb38fe9ed872237b7d51733610
Kris Norful <krisnorful@gmail.com>
2021-04-01 17:30:08 -0500

    create quizzesSlice file with reducer exports

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "react-redux";
+
+export const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState: {
+    quizzes: {}
+  },
+  reducers: {
+    addQuiz: (state, action) => {
+      state.quizzes[action.payload.id] = action.payload;
+    }
+  }
+});
+
+export const selectQuizzes = (state) => state.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
